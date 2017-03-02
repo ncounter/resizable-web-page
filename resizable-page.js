240bdbaa34ea31e70155a2ebab6895027a86fc86
Dario Leidi <dleidi@suse.com>
2017-03-02 13:41:21 +0100

    Add function to read from cookie the given key

--- resizable-page.js
+++ resizable-page.js
@@ -51,4 +51,20 @@ function onDocumentReady() {
     document.cookie = "asideWidth=" + asideWidth;
     document.cookie = "sectionWidth=" + sectionWidth;
   }
+
+  function readFromCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+  }
 }
