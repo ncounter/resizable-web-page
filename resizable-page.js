c50c86f9dee5bad9ca4b3814494ca1425e5a3c2a
Dario Leidi <dleidi@suse.com>
2017-03-02 13:31:26 +0100

    set heights dinamically

--- resizable-page.js
+++ resizable-page.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', onDocumentReady, false);
 
 function onDocumentReady() {
+  var header = document.querySelector('header');
+  var footer = document.querySelector('footer');
   var aside = document.querySelector('aside');
   var section = document.querySelector('section');
   var resizer = document.querySelector('.vertical-resizer');
@@ -31,5 +33,14 @@ function onDocumentReady() {
     var asidePercentage = Math.round(parseInt(document.defaultView.getComputedStyle(aside).width, 10) * 100 / window.innerWidth);
     aside.style.width = asidePercentage + '%';
     section.style.width = (100 - asidePercentage) + '%';
+
+    setHeights();
   }
+
+  function setHeights() {
+    var contentHeight = window.innerHeight - parseInt(document.defaultView.getComputedStyle(header).height, 10) - parseInt(document.defaultView.getComputedStyle(footer).height, 10) + 'px';
+    aside.style.height = section.style.height = contentHeight;
+    aside.style.top = section.style.top = parseInt(document.defaultView.getComputedStyle(header).height, 10) + 'px';
+  }
+  setHeights();
 }
